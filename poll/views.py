e58b45fd190e8f122b1a126a8fc7f5c78e0ed867
parsa-m1 <parsa.work0000@gmail.com>
2024-09-15 13:28:04 +0330

    poll detail added

--- poll/views.py
+++ poll/views.py
@@ -10,6 +10,13 @@ def index(request):
 def poll_detail(request, id):
     question = get_object_or_404(Question, id=id)
 
+    if request.method == 'POST':
+        voted = request.POST['voted']
+        choice = get_object_or_404(Choice, id=int(voted))
+        choice.vote += 1
+        choice.save()
+
+
     return render(request, 'poll_detail.html', {'question': question})
 
 def poll_results(request, q_id):
